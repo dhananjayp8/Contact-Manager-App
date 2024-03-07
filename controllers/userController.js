ad0af2bbdd6f624dd90e34bb761060ffd85ba4ed
Dhananjay Puranik <dhananjaypuranik@Dhananjays-MacBook-Pro.local>
2024-03-07 23:29:31 +0530

    added jwt acess token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 
 const asyncHandler=require("express-async-handler");
 const bcrypt=require('bcrypt');
+const jwt=require("jsonwebtoken");
 const User=require("../models/userModel");
 const registerUser=asyncHandler(async(req,res)=>{
     const {username,email,password}=req.body;
@@ -25,6 +26,9 @@ const registerUser=asyncHandler(async(req,res)=>{
     console.log(`User created ${user}`);
     if(user){
         res.status(201).json({_id:user.id,email:user.email});
+    }else{
+        res.status(400);
+        throw new Error("User data is not valid");
     }
     res.json({message:"Register the user"});
 });
@@ -32,8 +36,34 @@ const registerUser=asyncHandler(async(req,res)=>{
 
   
 const loginUser=asyncHandler(async(req,res)=>{
-    res.json({message:"Login user"});
-})
+
+    const {email,password}=req.body;
+    if(!email||!password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user =await User.findOne({email});
+    //compare password with hashpassword
+
+    if(user && (await bcrypt.compare(password,user.password))){
+        const accessToken =jwt.sign({
+            user:{
+                username:user.username,
+                email:user.email,
+                id:user.id,
+            },
+
+        },process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:"15m"}
+        );
+        res.status(200).json({accessToken});
+    }else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
+    //res.json({message:"Login user"});
+});
+
 const currentUser=asyncHandler(async(req,res)=>{
     res.json({message:"Current user information"});
 })
